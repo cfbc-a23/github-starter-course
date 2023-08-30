87bc50961570e927480044dac0df4b13c18e7419
jonclindaniel <jclindaniel@uchicago.edu>
2023-08-30 10:58:22 -0500

    Add example Hello World scripts

---
+++ 03-first-python-program/example.py
@@ -0,0 +1,11 @@
+# Single-line comment example with `#`
+
+'''
+Multi-line comment example with three apostrophes (') or quotation marks (")
+Execute this file from the terminal with the following command:
+python example.py
+
+Note that neither of the comments are printed out!
+'''
+
+print("Hello, world!")
